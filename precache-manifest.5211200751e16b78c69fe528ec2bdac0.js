a7a422248dc2b09e67bc4ff71d114fb1274dc110
ovzer <ovzer@users.noreply.github.com>
2020-05-12 00:14:01 +0000

    Deploying to gh-pages from  @ 308531e5454767a61eaa94fbd77b1dd5d1c397d4 🚀

---
+++ precache-manifest.5211200751e16b78c69fe528ec2bdac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70a4f314b6351a0abc910c4ee093414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e4b420043c7b510e7a6",
+    "url": "/static/css/main.84349ef8.chunk.css"
+  },
+  {
+    "revision": "b8a24ff001dedc74134b",
+    "url": "/static/js/2.30b76bdc.chunk.js"
+  },
+  {
+    "revision": "1258a30aff9d519bdd68b964627e3f2f",
+    "url": "/static/js/2.30b76bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4b420043c7b510e7a6",
+    "url": "/static/js/main.82456f50.chunk.js"
+  },
+  {
+    "revision": "49629580862fce500459",
+    "url": "/static/js/runtime-main.3ce1995a.js"
+  }
+]);
