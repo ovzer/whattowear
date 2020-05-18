8edc5495381da22be2d58284ce0976f0593ee90f
Ove Baardsgaard <ove@baardsgaard.com>
2020-05-18 23:32:18 +0200

    Make transition when changing location smoother

--- src/functions/getSunAltitude.ts
+++ src/functions/getSunAltitude.ts
@@ -2,7 +2,7 @@ import sunCalc from 'suncalc';
 
 import { ISimpleCoordinates } from './getAutocompleteSuggestions';
 
-export const getSunAltitude = async(coordinates: ISimpleCoordinates): Promise<number> => {
+export const getSunAltitude = (coordinates: ISimpleCoordinates): number => {
   const sunPosition = sunCalc.getPosition(new Date(), coordinates.lat, coordinates.lon);
   return sunPosition.altitude * 180 / Math.PI;
 };
--- src/functions/getWeather.ts
+++ src/functions/getWeather.ts
@@ -2,9 +2,12 @@ import { parseStringPromise } from 'xml2js';
 import moment from 'moment';
 
 import { ISimpleCoordinates } from './getAutocompleteSuggestions';
+import { getSunAltitude } from './getSunAltitude';
+import { getApparentTemperature } from './getApparentTemperature';
 
 export interface IWeather {
   temperature: number;
+  apparentTemperature: number;
   cloudiness: number;
   humidity: number;
   windSpeed: number;
@@ -22,19 +25,26 @@ export const getWeather = async(coordinates: ISimpleCoordinates): Promise<IWeath
     const text = await response.text();
     const result = await parseStringPromise(text);
     const forecastArray = result.weatherdata.product[0].time;
+    const sunAltitude = getSunAltitude(coordinates);
     const currentForecast = forecastArray.filter((forecast: any) => {
       return !moment().endOf('hour').diff(moment(forecast.$.from), 'minutes');
     }).reduce((returnArray: any, forecast: any) => {
       return { ...forecast.location[0], ...returnArray };
     }, {});
 
-    return {
+    const weather: IWeather = {
       temperature: +currentForecast.temperature[0].$.value,
+      apparentTemperature: 0,
       cloudiness: +currentForecast.cloudiness[0].$.percent,
       humidity: +currentForecast.humidity[0].$.value,
       windSpeed: +currentForecast.windSpeed[0].$.mps,
       precipitation: +currentForecast.precipitation[0].$.value,
       symbol: parseInt(currentForecast.symbol[0].$.number),
+    };
+
+    return {
+      ...weather,
+      apparentTemperature: getApparentTemperature(weather, sunAltitude),
     };
   } catch (error) {
     console.error(error);
